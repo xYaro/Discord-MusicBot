d2d5e7772218f4e4624ea1dbcbd73146bebe2ac2
xYaro <raven37t@gmail.com>
2021-09-21 18:41:01 -0500

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -1,12 +1,12 @@
 module.exports = {
   Admins: ["UserID", "UserID"], //Admins of the bot
   ExpressServer: true,//If you wanted to make the website run or not
-  DefaultPrefix: process.env.Prefix || ">", //Default prefix, Server Admins can change the prefix
+  DefaultPrefix: process.env.Prefix || "!", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/sbySMS7m3v", //Support Server Link
-  Token: process.env.Token || "", //Discord Bot Token
-  ClientID: process.env.Discord_ClientID || "", //Discord Client ID
-  ClientSecret: process.env.Discord_ClientSecret || "", //Discord Client Secret
+  Token: process.env.Token || "ODkwMDE1NzcwMTY2MzE3MTA2.YUppxw.owAq-WHxn_PzpXuvNifzHEBv7fg", //Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "890015770166317106", //Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "mPg9je50y-9_SaDQs9rFIrbQLLoam0J-", //Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
   CallbackURL: "/api/callback", //Discord OAuth2 Callback URL
   "24/7": false, //If you want the bot to be stay in the vc 24/7
